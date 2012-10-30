0c0d09c32e971661b6f4262fb3e2771bfca40565
Michael Zangerle <michael.zangerle@gmail.com>
2012-10-30 18:21:59 +0100

    main

---
+++ svm.logic.abstraction/src/svm/logic/abstraction/SearchControllerMain.java
@@ -0,0 +1,13 @@
+package svm.logic.abstraction;
+
+/**
+ * Projectteam: Team C
+ * Date: 30.10.12
+ */
+public class SearchControllerMain {
+
+    public static void main(String[] args) {
+
+    }
+
+}
