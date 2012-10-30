86bd4035bed2e072a1313ad9591bc947d85486c2
Michael Zangerle <michael.zangerle@gmail.com>
2012-10-30 23:17:16 +0100

    fix imports

--- svm.logic.abstraction/src/svm/logic/abstraction/controller/ITeamContestController.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/controller/ITeamContestController.java
@@ -1,5 +1,8 @@
 package svm.logic.abstraction.controller;
 
+import svm.logic.abstraction.transferobjects.ITransferMatch;
+import svm.logic.abstraction.transferobjects.ITransferTeam;
+
 /**
  * ProjectTeam: Team C
  * Date: 30.10.12
