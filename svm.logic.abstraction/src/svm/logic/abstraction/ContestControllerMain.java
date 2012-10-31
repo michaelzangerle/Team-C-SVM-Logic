310e70a5d5bc1fb69d64433eec95e6fbad6d68a8
Michael Zangerle <michael.zangerle@gmail.com>
2012-10-31 13:50:14 +0100

    contest controller main (1)

--- svm.logic.abstraction/src/svm/logic/abstraction/ContestControllerMain.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/ContestControllerMain.java
@@ -1,8 +1,18 @@
 package svm.logic.abstraction;
 
+import svm.logic.abstraction.controller.IContestController;
+import svm.logic.implementation.controller.ContestController;
+
 /**
  * Projectteam: Team C
  * Date: 31.10.12
  */
 public class ContestControllerMain {
+
+    public static void main(String[] args) {
+
+        IContestController contestController = new ContestController();
+
+    }
+
 }
