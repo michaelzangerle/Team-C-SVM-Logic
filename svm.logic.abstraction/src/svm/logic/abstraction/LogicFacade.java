1c92714f4d6bb4430b12e3342c63f88781e3d9cd
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-01 13:23:41 +0100

    Add Sub Team Controller to Factory and Facade

--- svm.logic.abstraction/src/svm/logic/abstraction/LogicFacade.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/LogicFacade.java
@@ -2,8 +2,12 @@ package svm.logic.abstraction;
 
 import svm.logic.abstraction.controller.IContestConfirmationController;
 import svm.logic.abstraction.controller.IContestController;
+import svm.logic.abstraction.controller.ISubTeamConfirmationController;
+import svm.logic.abstraction.controller.ISubTeamController;
 import svm.logic.abstraction.transferobjects.ITransferContest;
 import svm.logic.abstraction.transferobjects.ITransferMember;
+import svm.logic.abstraction.transferobjects.ITransferSubTeam;
+import svm.logic.abstraction.transferobjects.ITransferTeam;
 import svm.logic.implementation.ControllerFactory;
 import svm.persistence.abstraction.exceptions.NoSessionFoundException;
 
@@ -46,5 +50,36 @@ public class LogicFacade {
      */
     public static IContestConfirmationController getContestConfirmationController(ITransferMember member) {
         return ControllerFactory.getInstance().getContestConfirmationController(member);
+    }
+
+    /**
+     * Sub Team Confirmation Controller
+     * @return ISubTeamConfirmationController
+     */
+    public static ISubTeamConfirmationController getSubTeamConfirmationController ()
+    {
+        return ControllerFactory.getInstance().getSubTeamConfirmationController();
+    }
+
+    /**
+     * SubTeam Controller
+     * @param subTeam  ITransferSubTeam
+     * @return  ISubTeamController
+     */
+    public static ISubTeamController getSubTeamController (ITransferSubTeam subTeam){
+        return ControllerFactory.getInstance().getSubTeamController(subTeam);
+    }
+
+    /**
+     *    Subteam Controller
+     * @param team     ITransferTeam
+     * @param contest  ITransferContest
+     * @return  ISubTeamController
+     * @throws NoSessionFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
+    public static ISubTeamController getSubTeamController (ITransferTeam team,ITransferContest contest) throws NoSessionFoundException, IllegalAccessException, InstantiationException {
+        return ControllerFactory.getInstance().getSubTeamController(team,contest);
     }
 }
--- svm.logic.implementation/src/svm/logic/implementation/ControllerFactory.java
+++ svm.logic.implementation/src/svm/logic/implementation/ControllerFactory.java
@@ -3,13 +3,17 @@ package svm.logic.implementation;
 import svm.domain.abstraction.DomainFacade;
 import svm.domain.abstraction.modelInterfaces.IContest;
 import svm.domain.abstraction.modelInterfaces.IMember;
+import svm.domain.abstraction.modelInterfaces.ISubTeam;
+import svm.domain.abstraction.modelInterfaces.ITeam;
 import svm.logic.abstraction.controller.IContestConfirmationController;
 import svm.logic.abstraction.controller.IContestController;
-import svm.logic.abstraction.transferobjects.IHasModel;
-import svm.logic.abstraction.transferobjects.ITransferContest;
-import svm.logic.abstraction.transferobjects.ITransferMember;
+import svm.logic.abstraction.controller.ISubTeamConfirmationController;
+import svm.logic.abstraction.controller.ISubTeamController;
+import svm.logic.abstraction.transferobjects.*;
 import svm.logic.implementation.controller.ContestConfirmationController;
 import svm.logic.implementation.controller.ContestController;
+import svm.logic.implementation.controller.SubTeamConfirmationController;
+import svm.logic.implementation.controller.SubTeamController;
 import svm.persistence.abstraction.exceptions.NoSessionFoundException;
 
 /**
@@ -61,5 +65,19 @@ public class ControllerFactory {
      */
     public IContestConfirmationController getContestConfirmationController(ITransferMember member) {
         return new ContestConfirmationController(((IHasModel<IMember>) member).getModel());
+    }
+
+    public ISubTeamConfirmationController getSubTeamConfirmationController ()
+    {
+        return new SubTeamConfirmationController();
+    }
+
+    public ISubTeamController getSubTeamController(ITransferSubTeam subTeam)
+    {
+        return new SubTeamController(((IHasModel<ISubTeam>) subTeam).getModel());
+    }
+
+    public ISubTeamController getSubTeamController(ITransferTeam team, ITransferContest contest) throws NoSessionFoundException, InstantiationException, IllegalAccessException {
+        return new SubTeamController(((IHasModel<ITeam>) team).getModel(),((IHasModel<IContest>) contest).getModel());
     }
 }
