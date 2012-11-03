65bc976cbdc3e7dea7cb585b563af4f904c05246
Michael Zangerle <michael.zangerle@gmail.com>
2012-11-03 14:55:15 +0100

    make custom exceptions serializeable because of rmi?

--- svm.logic.abstraction/src/svm/logic/abstraction/exception/LogicException.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/exception/LogicException.java
@@ -1,10 +1,13 @@
 package svm.logic.abstraction.exception;
 
+import java.io.Serializable;
+import java.rmi.Remote;
+
 /**
  * ProjectTeam: Team C
  * Date: 01.11.12
  */
-public class LogicException extends Exception {
+public class LogicException extends Exception implements Serializable,Remote {
     public LogicException(String message) {
         super(message);
     }
