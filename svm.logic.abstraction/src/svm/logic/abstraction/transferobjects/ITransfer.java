a12f849663c8f2b471cd4749ededb66684c5877b
Michael Zangerle <michael.zangerle@gmail.com>
2012-11-03 10:42:10 +0100

    added Remote interface to ITransfer

--- svm.logic.abstraction/src/svm/logic/abstraction/transferobjects/ITransfer.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/transferobjects/ITransfer.java
@@ -1,13 +1,14 @@
 package svm.logic.abstraction.transferobjects;
 
 import java.io.Serializable;
+import java.rmi.Remote;
 
 /**
  * Projectteam
  * Date: 24.10.12
  * This Class consists all methods that are needed from the Transfer Objects
  */
-public interface ITransfer extends Serializable {
+public interface ITransfer extends Serializable,Remote {
 
     /**
      * Needed to set the Internal Object (Person,Match) from the Transfer Object with the invoke Method.
