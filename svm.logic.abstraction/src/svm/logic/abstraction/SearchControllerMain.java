5227dce3b255dd5be475410b70c268284bec89fc
johannes <johannes@greencode.at>
2012-11-04 11:36:48 +0100

    Member BirthDate Bug fixed

--- svm.logic.abstraction/src/svm/logic/abstraction/SearchControllerMain.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/SearchControllerMain.java
@@ -1,13 +1,33 @@
 package svm.logic.abstraction;
 
+import svm.logic.abstraction.controller.ISearchController;
+import svm.logic.abstraction.exception.IllegalGetInstanceException;
+import svm.logic.abstraction.transferobjects.ITransferMember;
+import svm.persistence.abstraction.exceptions.ExistingTransactionException;
+import svm.persistence.abstraction.exceptions.NoSessionFoundException;
+import svm.persistence.abstraction.exceptions.NoTransactionException;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Projectteam: Team C
  * Date: 30.10.12
  */
 public class SearchControllerMain {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IllegalGetInstanceException, NoSessionFoundException, ExistingTransactionException, NoTransactionException {
+        ISearchController search = LogicFacade.getSearchController();
+        search.start();
+        int date = 1;
+        int month = Calendar.SEPTEMBER;
+        int year = 1953;
+        Date d = new GregorianCalendar(year, month, date).getTime();
+        for (ITransferMember member : search.getMembers(d, new Date())) {
+            System.out.println(member.getFirstName() + " " + member.getLastName());
+        }
+        search.commit();
     }
 
 }
