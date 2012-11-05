36eb6ce93aa0f89681cda9a4fcf2573368c5c467
johannes <johannes@greencode.at>
2012-11-05 10:46:58 +0100

    TransferModel Team, Internal, External

--- svm.logic.abstraction/src/svm/logic/abstraction/transferobjects/ITransferExternalTeam.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/transferobjects/ITransferExternalTeam.java
@@ -4,6 +4,5 @@ package svm.logic.abstraction.transferobjects;
  * Projectteam: Team C
  * Date: 30.10.12
  */
-public interface ITransferTeam extends ITransfer {
-    String getName();
+public interface ITransferExternalTeam extends ITransfer {
 }
---
+++ svm.logic.implementation/src/svm/logic/implementation/tranferobjects/TransferInternalTeam.java
@@ -0,0 +1,32 @@
+package svm.logic.implementation.tranferobjects;
+
+import svm.domain.abstraction.modelInterfaces.ITeam;
+import svm.logic.abstraction.transferobjects.IHasModel;
+import svm.logic.abstraction.transferobjects.ITransferTeam;
+
+/**
+ * Projectteam: Team C
+ * Date: 05.11.12
+ */
+public class TransferInternalTeam implements ITransferTeam, IHasModel<ITeam> {
+    ITeam teamEntity;
+
+    public TransferInternalTeam(ITeam teamEntity) {
+        this.teamEntity = teamEntity;
+    }
+
+    @Override
+    public String getName() {
+        return teamEntity.getName();
+    }
+
+    @Override
+    public ITeam getModel() {
+        return teamEntity;
+    }
+
+    @Override
+    public void setObject(Object o) {
+        this.teamEntity = (ITeam) o;
+    }
+}
