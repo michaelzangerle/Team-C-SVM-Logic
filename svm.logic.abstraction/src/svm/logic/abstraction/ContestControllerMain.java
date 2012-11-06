4ec51db70d1a13951eb34c48745382540f7b777a
Michael Zangerle <michael.zangerle@gmail.com>
2012-11-06 23:34:04 +0100

    contest controller

--- svm.logic.abstraction/src/svm/logic/abstraction/ContestControllerMain.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/ContestControllerMain.java
@@ -2,11 +2,15 @@ package svm.logic.abstraction;
 
 import svm.domain.abstraction.exception.DomainAttributeException;
 import svm.domain.abstraction.exception.DomainParameterCheckException;
+import svm.domain.abstraction.modelInterfaces.IContest;
 import svm.logic.abstraction.controller.IContestController;
+import svm.logic.abstraction.controller.ILoginController;
 import svm.logic.abstraction.controller.ISearchController;
 import svm.logic.abstraction.exception.IllegalGetInstanceException;
+import svm.logic.abstraction.transferobjects.ITransferContest;
 import svm.logic.abstraction.transferobjects.ITransferLocation;
 import svm.logic.abstraction.transferobjects.ITransferMember;
+import svm.logic.abstraction.transferobjects.ITransferTeam;
 import svm.persistence.abstraction.exceptions.ExistingTransactionException;
 import svm.persistence.abstraction.exceptions.NoSessionFoundException;
 import svm.persistence.abstraction.exceptions.NoTransactionException;
@@ -22,36 +26,45 @@ public class ContestControllerMain {
 
     public static void main(String[] args) throws InstantiationException, IllegalAccessException, DomainParameterCheckException, DomainAttributeException, IllegalGetInstanceException, NoSessionFoundException, ExistingTransactionException, NoTransactionException, RemoteException {
 
-        ITransferMember user = LogicFacade.getLoginController().getMember();
+        ILoginController lc = LogicFacade.getLoginController();
+        lc.start();
+        lc.login("mary.sluis","");
+        ITransferMember user = lc.getMember();
+        lc.abort();
 
         ISearchController searchController = LogicFacade.getSearchController(user);
         searchController.start();
 
-        ITransferLocation location = searchController.getLocations().get(5);
+        ITransferContest contest = searchController.getContests().get(0);
+        searchController.abort();
 
-        IContestController contestController = LogicFacade.getContestController(user);
+        IContestController contestController = LogicFacade.getContestController(user,contest);
         contestController.start();
 
-        contestController.setContestStartDate(new Date());
-        contestController.setContestEndDate(new Date());
-        contestController.setContestName("Testcontest3");
-        contestController.setContestFee(150);
+//        contestController.setContestStartDate(new Date());
+//        contestController.setContestEndDate(new Date());
+//        contestController.setContestName("Testcontest3");
+//        contestController.setContestFee(150);
+//
+//        contestController.setEmail1("michael.zangerle@outlook.com");
+//        contestController.setEmail2("michael.zangerle@gmail.com");
+//
+//        contestController.setFax("0654 123 789");
+//
+//        contestController.setLat("4");
+//        contestController.setLong("10");
+//
+//        contestController.setStreet("Dorf");
+//        contestController.setStreetNumber("45");
+//
+//        contestController.setPhone1("0654 123 564 78");
+//        contestController.setPhone2("0654 123 564 79");
+//
+//        contestController.setLocation(location);
 
-        contestController.setEmail1("michael.zangerle@outlook.com");
-        contestController.setEmail2("michael.zangerle@gmail.com");
-
-        contestController.setFax("0654 123 789");
-
-        contestController.setLat("4");
-        contestController.setLong("10");
-
-        contestController.setStreet("Dorf");
-        contestController.setStreetNumber("45");
-
-        contestController.setPhone1("0654 123 564 78");
-        contestController.setPhone2("0654 123 564 79");
-
-        contestController.setLocation(location);
+        for (ITransferTeam c : contestController.getTeams()){
+            System.out.println(c.getName());
+        }
 
         contestController.commit();
 
