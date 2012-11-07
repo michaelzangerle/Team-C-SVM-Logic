14e4f7e3d62b29ea14a3ad875959fdd90fb7d649
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-07 15:04:27 +0100

    Search privilegs added + addPrivil and removePrivi

--- svm.logic.implementation/src/svm/logic/implementation/controller/SearchController.java
+++ svm.logic.implementation/src/svm/logic/implementation/controller/SearchController.java
@@ -5,6 +5,7 @@ import svm.domain.abstraction.exception.DomainParameterCheckException;
 import svm.domain.abstraction.modelInterfaces.*;
 import svm.logic.abstraction.controller.ISearchController;
 import svm.logic.abstraction.exception.IllegalGetInstanceException;
+import svm.logic.abstraction.exception.NotAllowException;
 import svm.logic.abstraction.transferobjects.*;
 import svm.logic.implementation.tranferobjects.*;
 import svm.logic.implementation.transferobjectcreator.TransferObjectCreator;
@@ -50,16 +51,23 @@ public class SearchController implements ISearchController {
      * @throws IllegalGetInstanceException
      */
     @Override
-    public List<ITransferMember> getMembers(String firstName, String lastName, ITransferDepartment department) throws NoSessionFoundException, IllegalGetInstanceException {
+    public List<ITransferMember> getMembers(String firstName, String lastName, ITransferDepartment department) throws NoSessionFoundException, IllegalGetInstanceException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferMember> result = new LinkedList<ITransferMember>();
         for (IMember member : DomainFacade.getMemberModelDAO().get(sessionId, firstName, lastName, ((IHasModel<IDepartment>) department).getModel())) {
             result.add((ITransferMember) TransferObjectCreator.getInstance(TransferMember.class, member));
         }
         return result;
+
     }
 
     @Override
-    public List<ITransferMember> getMembers(String firstName, String lastName, ITransferDepartment department, Boolean paid) throws NoSessionFoundException, IllegalGetInstanceException, RemoteException, DomainParameterCheckException {
+    public List<ITransferMember> getMembers(String firstName, String lastName, ITransferDepartment department, Boolean paid) throws NoSessionFoundException, IllegalGetInstanceException, RemoteException, DomainParameterCheckException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferMember> result = new LinkedList<ITransferMember>();
         if (department != null) {
             for (IMember member : DomainFacade.getMemberModelDAO().get(sessionId, firstName, lastName, ((IHasModel<IDepartment>) department).getModel())) {
@@ -85,7 +93,10 @@ public class SearchController implements ISearchController {
      * @throws IllegalGetInstanceException
      */
     @Override
-    public List<ITransferMember> getMembers(String firstName, String lastName) throws NoSessionFoundException, IllegalGetInstanceException {
+    public List<ITransferMember> getMembers(String firstName, String lastName) throws NoSessionFoundException, IllegalGetInstanceException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferMember> result = new LinkedList<ITransferMember>();
         for (IMember member : DomainFacade.getMemberModelDAO().get(sessionId, firstName, lastName)) {
             result.add((ITransferMember) TransferObjectCreator.getInstance(TransferMember.class, member));
@@ -101,7 +112,10 @@ public class SearchController implements ISearchController {
      * @throws IllegalGetInstanceException
      */
     @Override
-    public List<ITransferMember> getMembers(Date birthDateFrom, Date birthDateTo) throws NoSessionFoundException, IllegalGetInstanceException {
+    public List<ITransferMember> getMembers(Date birthDateFrom, Date birthDateTo) throws NoSessionFoundException, IllegalGetInstanceException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferMember> result = new LinkedList<ITransferMember>();
         for (IMember member : DomainFacade.getMemberModelDAO().get(sessionId, birthDateFrom, birthDateTo)) {
             result.add((ITransferMember) TransferObjectCreator.getInstance(TransferMember.class, member));
@@ -114,7 +128,10 @@ public class SearchController implements ISearchController {
      * @throws IllegalGetInstanceException
      */
     @Override
-    public List<ITransferDepartment> getDepartments() throws IllegalGetInstanceException, NoSessionFoundException {
+    public List<ITransferDepartment> getDepartments() throws IllegalGetInstanceException, NoSessionFoundException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferDepartment> result = new LinkedList<ITransferDepartment>();
         for (IDepartment department : DomainFacade.getDepartmentModelDAO().getAll(sessionId)) {
             result.add((ITransferDepartment) TransferObjectCreator.getInstance(TransferDepartment.class, department));
@@ -127,7 +144,10 @@ public class SearchController implements ISearchController {
      * @throws IllegalGetInstanceException
      */
     @Override
-    public List<ITransferLocation> getLocations() throws IllegalGetInstanceException, NoSessionFoundException {
+    public List<ITransferLocation> getLocations() throws IllegalGetInstanceException, NoSessionFoundException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferLocation> result = new LinkedList<ITransferLocation>();
 
         for (ILocation location : DomainFacade.getLocationModelDAO().get(sessionId, "AT", "Vorarlberg")) {
@@ -137,7 +157,10 @@ public class SearchController implements ISearchController {
     }
 
     @Override
-    public List<ITransferContest> getContests() throws IllegalGetInstanceException, NoSessionFoundException {
+    public List<ITransferContest> getContests() throws IllegalGetInstanceException, NoSessionFoundException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferContest> result = new LinkedList<ITransferContest>();
 
         for (IContest contest : DomainFacade.getContestModelDAO().getAll(sessionId)) {
@@ -147,7 +170,10 @@ public class SearchController implements ISearchController {
     }
 
     @Override
-    public List<ITransferTeam> getTeams() throws IllegalGetInstanceException, NoSessionFoundException {
+    public List<ITransferTeam> getTeams() throws IllegalGetInstanceException, NoSessionFoundException, NotAllowException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
         List<ITransferTeam> result = new LinkedList<ITransferTeam>();
 
         for (ITeam team : DomainFacade.getTeamModelDAO().getAll(sessionId)) {
@@ -161,8 +187,17 @@ public class SearchController implements ISearchController {
     }
 
     @Override
-    public List<ITransferUserPrivilege> getUserPrivileges() {
-        // TODO
-        return null;
+    public List<ITransferUserPrivilege> getUserPrivileges() throws NotAllowException, IllegalGetInstanceException {
+        if(! user.isAllowedForSearching())
+            throw new NotAllowException("Wrong privilege");
+
+        List<ITransferUserPrivilege> result = new LinkedList<ITransferUserPrivilege>();
+
+
+        for (IUserPrivilege priv : DomainFacade.getUserPrivilegeDAO().getAll(sessionId)) {
+            result.add((ITransferUserPrivilege) TransferObjectCreator.getInstance(TransferUserPrivileges.class, priv));
+        }
+
+        return result;
     }
 }
