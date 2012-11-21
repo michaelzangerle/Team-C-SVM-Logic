021ebb798fd63ff8ad990636b8c34390621d04ae
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-21 11:04:00 +0100

    jars und messagecontroller

--- svm.logic.abstraction/src/svm/logic/abstraction/controller/IMessageController.java
+++ svm.logic.abstraction/src/svm/logic/abstraction/controller/IMessageController.java
@@ -1,12 +1,16 @@
 package svm.logic.abstraction.controller;
 
+import svm.logic.abstraction.transferobjects.ITransferMember;
+
 import java.io.Serializable;
 import java.rmi.Remote;
+import java.util.List;
 
 /**
  * Projectteam : Team C
  * Date: 21.11.12
  */
-public interface IMessageController extends Serializable, Remote {
+public interface IMessageController extends Serializable, Remote, IController {
 
+    List<TransferMessage> update(ITransferMember member);
 }
--- svm.logic.abstraction/src/svm/logic/abstraction/transferobjects/ITransferMessage.java
+++
@@ -1,9 +0,0 @@
-package svm.logic.abstraction.transferobjects;
-
-/**
- * Projectteam : Team C
- * Date: 21.11.12
- */
-public interface ITransferMessage extends ITransfer {
-
-}
