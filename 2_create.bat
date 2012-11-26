33d4f73e17c958b4c638353307bcb8ab3017d00b
johannes <johannes@greencode.at>
2012-11-26 16:38:50 +0100

    CREATE.BAT

--- 2_create.bat
+++ 2_create.bat
@@ -1,7 +1,7 @@
 @echo off
 set IMQ_HOME="C:\glassfishv3\mq"
 
-%IMQ_HOME%\bin\imqobjmgr add -t tf -l "svm:topicFactory"  -j java.naming.provider.url=file:///C:/temp -j java.naming.factory.initial=com.sun.jndi.fscontext.RefFSContextFactory -f
+%IMQ_HOME%\bin\imqobjmgr add -t tf -l "java:tf"  -j java.naming.provider.url=file:///C:/temp -j java.naming.factory.initial=com.sun.jndi.fscontext.RefFSContextFactory -f
 
 %IMQ_HOME%\bin\imqobjmgr add -t t -l "svm/subTeam" -o "imqDestinationName=svmsubTeam" -j java.naming.provider.url=file:///C:/temp -j java.naming.factory.initial=com.sun.jndi.fscontext.RefFSContextFactory -f
 %IMQ_HOME%\bin\imqobjmgr add -t t -l "svm/member" -o "imqDestinationName=svmmember" -j java.naming.provider.url=file:///C:/temp -j java.naming.factory.initial=com.sun.jndi.fscontext.RefFSContextFactory -f
